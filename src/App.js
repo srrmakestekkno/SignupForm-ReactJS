fc7d8e33505be00a335a919868530fea45a35428
stian røgeberg <srr007@hotmail.com>
2024-09-05 09:55:19 +0200

    Added form structure and empty css classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import "./styles/styles.css";
+
+const Input = ({ name, id, placeholder }) => {
+  return (
+    <>
+      <input type="text" name={name} id={id} placeholder={placeholder} />
+    </>
+  );
+};
+
+const Button = ({ children }) => {
+  return (
+    <>
+      <button className="button">{children}</button>
+    </>
+  );
+};
+
+const ErrorMessage = () => {
+  return (
+    <>
+      <p id="error-message"></p>
+    </>
+  );
+};
+
+const Form = () => {
+  return (
+    <>
+      <form className="form">
+        <div>
+          <label></label>
+          <Input
+            name="firstname"
+            id="firstname-input"
+            placeholder="Firstname"
+          />
+        </div>
+        <div>
+          <label></label>
+          <Input name="email" id="email-input" placeholder="Email" />
+        </div>
+        <div>
+          <label></label>
+          <Input name="password" id="password-input" placeholder="Password" />
+        </div>
+        <div>
+          <label></label>
+          <Input
+            name="repeatPassword"
+            id="repeat-password-input"
+            placeholder="Repeat password"
+          />
+        </div>
+        <Button>Signup</Button>
+      </form>
+    </>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>signup</h1>
+      <Form />
+      <ErrorMessage />
     </div>
   );
 }
